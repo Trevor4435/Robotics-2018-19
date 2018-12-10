52ff140f5137b78d085d244c84a4d60fc3c280ad
Trevor4435 <42450867+Trevor4435@users.noreply.github.com>
2018-12-10 15:43:44 -0500

    Update Drivebase.java

--- Drivebase.java
+++ Drivebase.java
@@ -3,10 +3,34 @@ package org.usfirst.frc.team3359.robot;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drivebase extends Subsystem {
+	
+	private TalonSRX LeftMotor2;
+	private TalonSRX LeftMotor4;
+	private TalonSRX RightMotor1;
+	private TalonSRX RightMotor3;
+	
+	public Drivebase() {
+		LeftMotor2 = new TalonSRX(RobotMap.LEFT_MOTOR2.value);
+		LeftMotor4 = new TalonSRX(RobotMap.LEFT_MOTOR4.value);
+		RightMotor1 = new TalonSRX(RobotMap.RIGHT_MOTOR1.value);
+		RightMotor3 = new TalonSRX(RobotMap.RIGHT_MOTOR3.value);
+		
+		Robot.initTalon(LeftMotor2);
+		Robot.initTalon(LeftMotor4);
+		Robot.initTalon(RightMotor1);
+		Robot.initTalon(RightMotor3);
+	}
+	
+	public void set(ControlMode mode, double leftvalue, double rightvalue) {
+		LeftMotor2.set(mode, leftvalue);
+		LeftMotor4.set(mode, leftvalue);
+		RightMotor1.set(mode, rightvalue);
+		RightMotor3.set(mode, rightvalue);
+	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
+		
 
 	}
 
